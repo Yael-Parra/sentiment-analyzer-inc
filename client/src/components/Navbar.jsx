1c575cd35091299afb106c7cec483643e60876be
jruizndev <jorsqn@gmail.com>
2025-06-26 14:16:33 +0200

    feature: initial frontend setup - fix #9

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <footer>
+      Soy la Navbar
+    </footer>
+  );
+};
+
+  export default Navbar;
---
+++ client/src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home.jsx';
+import LinkAnalysis from '../pages/LinkAnalysis.jsx';
+import Statistics from '../pages/Statistics.jsx';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/link-analysis" element={<LinkAnalysis />} />
+      <Route path="/statistics" element={<Statistics />} />
+      <Route path="*" element={<div>Not Found</div>} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
