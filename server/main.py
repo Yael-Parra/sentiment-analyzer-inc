0603f37b7d54b82128b5f5ad2b75d92914cc5684
Nho89 <noa1720@gmail.com>
2025-07-08 12:40:32 +0200

    feat(server): added API's  #10

--- server/main.py
+++ server/main.py
@@ -1,7 +1,8 @@
-# backend/app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from etl.youtube_extraction import extract_comments, VideoRequest, Comment
+from server.schemas import VideoRequest, Comment  
+from etl.youtube_extraction import extract_video_id, fetch_comment_threads 
+from server.outils.prediction_pipeline import predict_pipeline
 from typing import List
 
 app = FastAPI()
@@ -20,4 +21,24 @@ def read_root():
 
 @app.post("/extract-comments/", response_model=List[Comment])
 def extract_comments_endpoint(request: VideoRequest):
-    return extract_comments(request)
+    video_id = extract_video_id(request.url_or_id)
+    comments = fetch_comment_threads(video_id)
+    return comments
+
+# Endpoint predicción
+@app.post("/predict/")
+def predict_from_youtube(request: VideoRequest):
+    result = predict_pipeline(request.url_or_id, max_comments=request.max_comments)
+    return result
+
+
+# Endpoint GET para los gráficos, se traen por video_id:
+@app.get("/stats/")
+def get_stats(video_id: str):
+    stats = {
+        "video_id": video_id,
+        "positive_comments": 0,
+        "negative_comments": 0,
+        "neutral_comments": 0,
+    }
+    return stats
