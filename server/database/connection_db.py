71a5e72ec0b5bd0f2f9b7867350a5426cfed4506
Nho89 <noa1720@gmail.com>
2025-07-14 12:03:03 +0200

    refactor: add requirements streamlit and httpx

--- server/database/connection_db.py
+++ server/database/connection_db.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(override=True)
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
